c1efd4aa08fbf211bec50b0b692ad3205cff7644
josue <alejandro@MacBook-Air-de-josue.local>
2024-07-15 16:00:47 -0600

    Se inicializa el proyecto

---
+++ src/components/InvoiceApp.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const InvoiceApp = () => {
+    return (
+        <>
+            <h1>Ejemplo Factura</h1>
+        </>
+    )
+}
+
+export default InvoiceApp;
---
+++ src/data/factura.js
@@ -0,0 +1,37 @@
+export const factura = {
+    id: 10,
+    nombre: 'Componentes PC',
+    cliente: {
+        nombreCliente: 'Pepe',
+        apellido: 'Dee',
+        direccion: {
+            pais: 'USA', 
+            city: 'Los Angeles',
+            calle: 'Calle 1',
+            numero: 24
+        },
+    },
+    empresa: {
+        nombreEmpresa: 'New EF',
+        fiscalia: 12333
+    } ,
+    items: [
+        {
+            Producto: 'CPU',
+            precio: 499,
+            cantidad: 1
+        },
+
+        {
+            Producto: 'Celeron',
+            precio: 344,
+            cantidad: 2
+        },
+
+        {
+            Producto: 'AMD',
+            precio: 600,
+            cantidad: 3
+        } 
+    ]
+}
